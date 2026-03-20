1460e9babd14e5efb957e89603dcdcab1b5eb56c
daylenh <dayven.lenh@gmail.com>
2026-03-20 05:02:58 -0700

    Created a database connection

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -21,14 +21,14 @@ repositories {
 }
 
 dependencies {
-	// implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("tools.jackson.module:jackson-module-kotlin")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	// runtimeOnly("com.mysql:mysql-connector-j")
-	// testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+	runtimeOnly("com.mysql:mysql-connector-j")
+	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
--- backend/src/main/kotlin/edu/sjsu/courseplanner/backend/HealthController.kt
+++ backend/src/main/kotlin/edu/sjsu/courseplanner/backend/HealthController.kt
@@ -2,13 +2,27 @@ package edu.sjsu.courseplanner.backend
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.sql.DataSource
 
-// Returns a simple health-check response to confirm the backend is running
+// Returns a simple health-check response to confirm the backend & database is running
 @RestController
 @RequestMapping("/api")
-class HealthController {
+class HealthController(private val dataSource: DataSource) {
+    // test backend
     @GetMapping("/health")
     fun health(): Map<String, String> {
         return mapOf("status" to "ok")
+    }
+    // test db
+    @GetMapping("/db")
+    fun dbHealth(): Map<String, String> {
+        return try {
+            val conn = dataSource.connection
+            val status = if (!conn.isClosed) "connected" else "not connected"
+            conn.close()
+            mapOf("database" to status)
+        } catch (e: Exception) {
+            mapOf("database" to "error", "message" to (e.message ?: "unknown"))
+        }
     }
 }
